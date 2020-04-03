78e582cdb1bc83c778eb854e413f86969928ec08
Nick Gashkov <nick@gashkov.com>
2020-04-03 17:13:03 +0500

    Add 'config.New' builder

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestNewWithoutArgsAndEnviron(t *testing.T) {
+func TestNewWithDefaults(t *testing.T) {
 	var (
 		args    []string
 		environ []string
@@ -13,4 +13,28 @@ func TestNewWithoutArgsAndEnviron(t *testing.T) {
 
 	config := New(args, environ)
 	assert.Equal(t, *config, Config{pattern: ""})
+}
+
+func TestNewWithArgsInMultipleItems(t *testing.T) {
+	var args = []string{"--pattern", "*/**.py"}
+	var environ []string
+
+	config := New(args, environ)
+	assert.Equal(t, *config, Config{pattern: "*/**.py"})
+}
+
+func TestNewWithEnviron(t *testing.T) {
+	var args []string
+	var environ = []string{"TAGSPECTOR_PATTERN=*/**.py"}
+
+	config := New(args, environ)
+	assert.Equal(t, *config, Config{pattern: "*/**.py"})
+}
+
+func TestNewWithArgsHasPrecedenceOverEnviron(t *testing.T) {
+	var args = []string{"--pattern", "higher_priority.py"}
+	var environ = []string{"TAGSPECTOR_PATTERN=lower_priority.py"}
+
+	config := New(args, environ)
+	assert.Equal(t, *config, Config{pattern: "higher_priority.py"})
 }
